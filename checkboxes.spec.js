6d784aa65cb26365f99ebc892f57ece89f24e3c1
sugan1990 <116284772+sugan1990@users.noreply.github.com>
2024-02-17 23:49:10 +0530

    Delete checkboxes.spec.js

--- checkboxes.spec.js
+++
@@ -1,9 +0,0 @@
-import {test, expect} from '@playwright/test'
-
-test('checkbox', async ({page}) =>{
-
-    await page.goto("https://demoqa.com/automation-practice-form")
-    await page.locator("//input[@id='hobbies-checkbox-1' and @type='checkbox']").scrollIntoViewIfNeeded()
-    await page.locator("//input[@id='hobbies-checkbox-1' and @type='checkbox']").check()
-
-})
